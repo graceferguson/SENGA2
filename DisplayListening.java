88b72dcfa78d9278772230ddbc2e1f1fad90fe60
ksdhami <32716134+ksdhami@users.noreply.github.com>
2017-11-03 11:28:22 -0600

    need to know how logger will deal with messages

--- DisplayListening.java
+++ DisplayListening.java
@@ -22,7 +22,7 @@ public class DisplayListening implements DisplayListener {
 
 	@Override
 	public void messageChange(Display display, String oldMessage, String newMessage) {
-		
+		// write new message to logger 
 		
 	}
 	
