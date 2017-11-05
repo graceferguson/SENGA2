4c62e6e1793be3925566ee1ed8631b12443c4bbb
ksdhami <32716134+ksdhami@users.noreply.github.com>
2017-11-05 13:47:00 -0700

    Update VendCommunicator.java

--- VendCommunicator.java
+++ VendCommunicator.java
@@ -1,7 +1,7 @@
 //SENG300 Group Assignment 1
 //Tae Chyung (10139101), Cameron Davies (30003456) & Grace Ferguson (30004869)
 
-package ca.ucalgary.seng300.a1;
+package ca.ucalgary.seng300.a2;
 
 import org.lsmr.vending.hardware.*;
 
@@ -14,15 +14,17 @@ public class VendCommunicator {
 	private CoinReceptacleListening receptacle;
 	private PopCanRackListening[] pRacks;
 	private VendingMachine machine;
+	private DisplayListening displayListener;
 
 	public VendCommunicator() {
 	}
 
 	// Links the appropriate parts to their corresponding variables
-	public void linkVending(CoinReceptacleListening receptacle, PopCanRackListening[] pRacks, VendingMachine machine) {
+	public void linkVending(CoinReceptacleListening receptacle, PopCanRackListening[] pRacks, VendingMachine machine, DisplayListening display) {
 		this.receptacle = receptacle;
 		this.pRacks = pRacks;
 		this.machine = machine;
+		this.displayListener = display;
 	}
 
 	/**
